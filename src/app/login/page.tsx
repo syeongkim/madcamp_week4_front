64acc32935438a55cfb50aa40411e77fa271063c
seoyeong <carsk0607@gmail.com>
2024-07-20 23:02:34 +0900

    feat: add login page and login form

---
+++ src/app/login/page.tsx
@@ -0,0 +1,64 @@
+import Link from "next/link";
+import Logo from "../components/Logo";
+import Button from "../components/Button";
+import "./styles/login.css"; // Tailwind CSS는 별도로 구성되어야 함
+
+export default function Login() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="p-8 rounded-lg shadow-md w-full max-w-md">
+        <div className="mb-6 text-center">
+          <Logo />
+        </div>
+        <form className="space-y-6 font-Animales text-white">
+          <div>
+            <label htmlFor="username" className="block text-sm font-medium">
+              Username
+            </label>
+            <input
+              id="username"
+              name="username"
+              type="text"
+              autoComplete="username"
+              required
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium">
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                id="remember_me"
+                name="remember_me"
+                type="checkbox"
+                className="h-4 w-4 focus:ring-indigo-500 border-gray-300 rounded"
+              />
+              <label htmlFor="remember_me" className="ml-2 block text-sm">
+                Remember me
+              </label>
+            </div>
+          </div>
+          <div>
+            <Link href="/dorms">
+              <Button className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Sign in
+              </Button>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
