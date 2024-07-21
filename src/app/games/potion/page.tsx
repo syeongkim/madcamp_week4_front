47b6a2e30649f23dcdc1c3c02789140d259b7117
seoyeong <carsk0607@gmail.com>
2024-07-22 01:13:06 +0900

    feat: add potion making zone

--- src/app/games/potion/page.tsx
+++ src/app/games/potion/page.tsx
@@ -1,17 +1,51 @@
-import React from "react";
+'use client';
+
+import React, { useRef } from "react";
 import ingredients from "./ingredient.json";
+import "./styles/potion.css";
 
 const Potion: React.FC = () => {
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    const container = scrollContainerRef.current;
+    if (!container) return;
+
+    container.classList.add("scrolling");
+
+    const startX = e.pageX - container.offsetLeft;
+    const scrollLeft = container.scrollLeft;
+
+    const handleMouseMove = (e: MouseEvent) => {
+      const x = e.pageX - container.offsetLeft;
+      const walk = (x - startX) * 2; // 스크롤 속도 조정
+      container.scrollLeft = scrollLeft - walk;
+    };
+
+    const handleMouseUp = () => {
+      container.classList.remove("scrolling");
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseup", handleMouseUp);
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mouseup", handleMouseUp);
+  };
+
   return (
-    <div>
-      <h1 className="text-white font-Harry text-6xl text-center mb-6 mt-4">Potion Ingredients</h1>
-      <div className="grid grid-cols-5 gap-4">
+    <div className="min-h-screen bg-makepotions-background bg-cover bg-center">
+      <h1 className="text-gradient-blue font-Harry text-6xl text-center mb-6 mt-4">Potion</h1>
+      <div
+        className="flex overflow-x-auto whitespace-nowrap mb-6 no-scrollbar"
+        ref={scrollContainerRef}
+        onMouseDown={handleMouseDown}
+      >
         {ingredients.map((ingredient, index) => (
-          <div key={index} className="p-4 rounded-lg shadow-lg">
-            <div className="w-full h-40 relative overflow-hidden rounded-md mb-2">
+          <div key={index} className="p-1 rounded-lg shadow-lg inline-block">
+            <div className="w-20 h-20 relative overflow-hidden rounded-md mb-2">
               <img src={ingredient.imageUrl} alt={ingredient.name} className="absolute inset-0 w-full h-full object-cover object-center" />
             </div>
-            <h2 className="text-sm font-bold text-white font-Animales">{ingredient.name}</h2>
+            <h2 className="text-xxs font-bold text-white font-Animales text-center">{ingredient.name}</h2>
           </div>
         ))}
       </div>
