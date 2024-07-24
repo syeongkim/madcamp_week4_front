e5eca76825f2e8c612e406eb3ed6e66ce3d66ec9
seoyeong <carsk0607@gmail.com>
2024-07-25 03:25:24 +0900

    feat: add next.config.mjs

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const prefix =
-    process.env.NODE_ENV === 'production' ? 'https://syeongkim.github.io/madcamp_week4_front/' : ''
-
-const nextConfig = {
-    output: 'export',
-    assetPrefix: prefix,
-}
--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,28 @@
-export function webpack(config) {
+// next.config.mjs
+const prefix = process.env.NODE_ENV === 'production' ? '/madcamp_week4_front/' : '';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  assetPrefix: prefix,
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/madcamp_week4_front' : '',
+  webpack(config) {
     config.module.rules.push({
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: {
-            loader: 'file-loader',
-            options: {
-                name: '[name].[ext]',
-                outputPath: 'fonts/',
-            },
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'fonts/',
         },
+      },
     });
 
     return config;
-}
+  }
+};
+
+export default nextConfig;
