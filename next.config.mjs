555668540740efbefaa8bd9f4405a085b87675ff
kim-minkyoung <decomin02@pusan.ac.kr>
2024-07-25 05:35:30 +0900

    fix: base 순서

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,23 @@
-const prefix = process.env.NODE_ENV === 'production' ? '/madcamp_week4_front/' : '';
+const prefix =
+  process.env.NODE_ENV === "production" ? "/madcamp_week4_front/" : "";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   assetPrefix: prefix,
+  basePath: process.env.NODE_ENV === "production" ? "/madcamp_week4_front" : "",
   images: {
-    unoptimized: true,
+    unoptimized: true, // 이미지 최적화 비활성화
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/madcamp_week4_front' : '',
   webpack(config) {
     config.module.rules.push({
       test: /\.(woff|woff2|eot|ttf|otf)$/,
       use: {
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '[name].[ext]',
-          outputPath: 'fonts/',
+          name: "[name].[ext]",
+          outputPath: "fonts/",
+          publicPath: `${prefix}fonts/`, // 폰트의 공개 경로 설정
         },
       },
     });
