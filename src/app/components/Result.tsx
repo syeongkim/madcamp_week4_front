060004f903caa1695f1cdbd5ad4aed8123ccddc9
seoyeong <carsk0607@gmail.com>
2024-07-24 22:36:49 +0900

    feat: connect signup page api

--- src/app/components/Result.tsx
+++ src/app/components/Result.tsx
@@ -29,7 +29,7 @@ const Result: React.FC<QuestionProps> = ({ resultIndex }) => {
     <div className="box flex flex-col items-center justify-center font-Harry">
       <h2 className="title">{results[resultIndex].resultType}</h2>
       <p className="detail justify-center text-center mb-6">{results[resultIndex].resultDetail}</p>
-      <Link href="/signup">
+      <Link href={`/signup?dorm=${results[resultIndex].resultType}`}>
         <p className="text-white text-4xl font-large mt-6 hover:text-yellow-500">Go to Sign Up</p>
       </Link>
     </div>
